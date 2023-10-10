67a1d24742192509f4e94b63b2d7b3e40af7a303
robgodfrey <rob.godfrey@ft.com>
2023-10-10 14:19:52 +0100

    Test whether parameters are expanded in scripts

--- src/scripts/authenticate_with_oidc.sh
+++ src/scripts/authenticate_with_oidc.sh
@@ -2,23 +2,23 @@
 
 set +e
 
-if [ -z "$CLOUDSMITH_ORGANISATION" ]
+if [ -z "${<<parameters.organisation>>}" ]
 then
   echo "Unable to generate OIDC token. Environment variable CLOUDSMITH_ORGANISATION is not set."
   exit 1
 fi
 
-if [ -z "$CLOUDSMITH_SERVICE_ACCOUNT" ]
+if [ -z "${<<parameters.service_account}" ]
 then
   echo "Unable to generate OIDC token. Environment variable CLOUDSMITH_SERVICE_ACCOUNT is not set."
   exit 1
 fi
 
-echo "Generating Cloudsmith OIDC token for service account: $CLOUDSMITH_ORGANISATION/$CLOUDSMITH_SERVICE_ACCOUNT"
+echo "Generating Cloudsmith OIDC token for service account: ${<<parameters.organisation>>}/${<<parameters.service_account}"
 
 RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
-            -d "{\"oidc_token\":\"$CIRCLE_OIDC_TOKEN_V2\", \"service_slug\":\"$CLOUDSMITH_SERVICE_ACCOUNT\"}" \
-            --silent --show-error "https://api.cloudsmith.io/openid/$CLOUDSMITH_ORGANISATION/")
+            -d "{\"oidc_token\":\"$CIRCLE_OIDC_TOKEN_V2\", \"service_slug\":\"${<<parameters.organisation>>}\"}" \
+            --silent --show-error "https://api.cloudsmith.io/openid/${<<parameters.service_account}/")
 
 CLOUDSMITH_OIDC_TOKEN=$(echo "$RESPONSE" | grep -o '"token": "[^"]*' | grep -o '[^"]*$')
 
@@ -36,7 +36,5 @@ else
 
   echo "The following environment variables have been exported. The OIDC token has been masked below."
   echo ""
-  echo "CLOUDSMITH_ORGANISATON=$CLOUDSMITH_ORGANISATION"
-  echo "CLOUDSMITH_SERVICE_ACCOUNT=$CLOUDSMITH_SERVICE_ACCOUNT"
   echo "CLOUDSMITH_OIDC_TOKEN=********"
 fi
