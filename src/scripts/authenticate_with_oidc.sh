cc10adbd246b969ae3b5476f7dc8ad288da7b696
robgodfrey <rob.godfrey@ft.com>
2023-10-10 14:58:53 +0100

    Switch to using environment variables to pass organisation and service account

--- src/scripts/authenticate_with_oidc.sh
+++ src/scripts/authenticate_with_oidc.sh
@@ -7,17 +7,17 @@ service_account=$(eval echo "\$$CLOUDSMITH_SERVICE_ACCOUNT_ENV_VAR")
 
 if [ -z "$organisation" ]
 then
-  echo "Unable to generate OIDC token. Environment variable CLOUDSMITH_ORGANISATION is not set."
+  echo "Unable to generate OIDC token. Environment variable $CLOUDSMITH_ORGANISATION_ENV_VAR is not set."
   exit 1
 fi
 
 if [ -z "$service_account" ]
 then
-  echo "Unable to generate OIDC token. Environment variable CLOUDSMITH_SERVICE_ACCOUNT is not set."
+  echo "Unable to generate OIDC token. Environment variable $CLOUDSMITH_SERVICE_ACCOUNT_ENV_VAR is not set."
   exit 1
 fi
 
-echo "Generating Cloudsmith OIDC token for service account: $organisation/$service_account"
+echo "Generating Cloudsmith OIDC token for service account ..."
 
 RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
             -d "{\"oidc_token\":\"$CIRCLE_OIDC_TOKEN_V2\", \"service_slug\":\"$service_account\"}" \
@@ -32,12 +32,11 @@ then
   echo "$RESPONSE"
   exit 1
 else
-  echo "Successfully generated OIDC token."
-  echo ""
-
   echo "export CLOUDSMITH_OIDC_TOKEN=\"$CLOUDSMITH_OIDC_TOKEN\"" >> "$BASH_ENV"
 
-  echo "The following environment variables have been exported. The OIDC token has been masked below."
+  echo "Successfully generated OIDC token."
+  echo ""
+  echo "The following environment variable has been exported. The OIDC token value has been masked."
   echo ""
   echo "CLOUDSMITH_OIDC_TOKEN=********"
 fi
